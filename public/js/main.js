cb94e1099e59c6ec1391cc29b0ff89dfaae175fb
Vitor <vitorgabrielifpr@gmail.com>
2023-06-14 16:28:18 -0300

    Manipulação de DOM - v1.1
    Adição de versão 'dark' para melhor acessibilidade.
    
    - At.te

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ const listaPosts = [
         titulo: "Rinocerontinho",
         descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, non. Voluptatibus neque sequi obcaecati unde, voluptatum error adipisci laboriosam molestiae sit deleniti veniam in ea, ratione quas dolorum, omnis assumenda?!",
         imgURL: "./img/img3.jpg"
-    },
+    }
 ]
 
 let main = document.querySelector(".main")
@@ -33,7 +33,43 @@ const criarPost = () => {
         main.innerHTML += card
     })
 }
+let button = document.querySelector(".btTema")
 
 window.onload = () => {
     criarPost()
+
+    let cards = document.querySelectorAll(".card")
+    button.addEventListener("click", () => {
+
+        if(button.checked){
+            cards.forEach((card) => {
+                card.classList.add("card-dark")
+            })
+        }else{
+            cards.forEach((card) => {
+                card.classList.remove("card-dark")
+            })
+        }
+       
+    })
+    
 }
+
+button.addEventListener("click", () => {
+    let body = document.querySelector("body")
+    let header = document.querySelector("header")
+    let footer = document.querySelector("footer")
+
+    if(button.checked){
+        body.classList.add("body-dark")
+        header.classList.add("header_footer-dark")
+        footer.classList.add("header_footer-dark")
+    }
+    else{
+        body.classList.remove("body-dark")
+        header.classList.remove("header_footer-dark")
+        footer.classList.remove("header_footer-dark")
+    }
+    
+})
+
