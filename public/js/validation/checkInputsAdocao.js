665fa79a4fa54b019a91b3f59f7f580085ad3774
Vitor <vitorgabrielifpr@gmail.com>
2023-06-25 20:10:02 -0300

    Petshop - v3.5 |
    Criação de funcionalidades para a quantificação de inputs inválidos, que posteriormente será utilizado para indicar uma mensagem de erro.
    
    -At.te

--- public/js/validation/checkInputsAdocao.js
+++ public/js/validation/checkInputsAdocao.js
@@ -1,20 +1,57 @@
+import { gerarErroAdocao } from "../process/gerarErroAdocao.js"
 import * as elemento from "../utils/elementosAdocao.js"
 
 let radioBts = document.querySelectorAll(".radioBts")
+
+export const inputsValidosAdocao = () => {
+    let inputsValidos = {}
+    let cont = 0
+
+    if(elemento.nome.value.trim() == ""){
+        inputsValidos.nome = true      
+    }
+    if(elemento.telefone.value.trim() == ""){
+        inputsValidos.telefone = true      
+    }
+    if(elemento.email.value.trim() == ""){
+        inputsValidos.email = true
+    }
+    inputsValidos.radiobuttons = false
+
+    radioBts.forEach((btn) => {
+        if(!btn.checked){
+            cont++
+        }
+        if(cont == 3) inputsValidos.radiobuttons = true
+    })
+
+    if(elemento.residencia.value == "0"){
+        inputsValidos.residencia = true
+    }
+    if(elemento.intencao.value.trim() == ""){
+        inputsValidos.intencao = true  
+    }
+
+    return inputsValidos
+
+}
 
 export const checkInputsAdocao = () => {
     let control = true
     let cont = 0
     if(elemento.nome.value.trim() == ""){
         elemento.nome.classList.add("erro")
+        elemento.nome.setAttribute('placeholder', "Nome inválido!")
         control = false        
     }
     if(elemento.telefone.value.trim() == ""){
         elemento.telefone.classList.add("erro")
+        elemento.telefone.setAttribute('placeholder', "Telefone inválido!")
         control = false        
     }
     if(elemento.email.value.trim() == ""){
         elemento.email.classList.add("erro")
+        elemento.email.setAttribute('placeholder', "E-mail inválido!")
         control = false
     }
 
@@ -26,13 +63,17 @@ export const checkInputsAdocao = () => {
     })
     
     if(elemento.residencia.value == "0"){
-        control = false   
+        elemento.residencia.classList.add("erro")
+        
+        control = false
     }
 
     if(elemento.intencao.value.trim() == ""){
-        control = false   
+        elemento.intencao.classList.add("erro")
+        elemento.intencao.setAttribute('placeholder', "Intenção inválida!")
+        control = false
     }
 
-
+    gerarErroAdocao()
     return control
 }
--- public/js/validation/checkInputsCadastro.js
+++ public/js/validation/checkInputsCadastro.js
@@ -1,16 +1,40 @@
+import { gerarErroCadastro } from "../process/gerarErroCadastro.js"
 import * as elemento from "../utils/elementosCadastro.js"
+
+export const inputsInvalidosCadastro = () => {
+    let inputsInvalidos = {}
+
+    if(elemento.titulo.value.trim() == ""){
+        inputsInvalidos.titulo = true 
+    }
+    if(elemento.descricao.value.trim() ==  ""){
+        inputsInvalidos.descricao = true
+    }
+    if(elemento.imgURL.value.trim() ==  ""){
+        inputsInvalidos.imgURL = true
+    }
+
+    return inputsInvalidos
+}
 
 export const checkInputsCadastro = () => {
     let control = true
 
     if(elemento.titulo.value.trim() ==  ""){
+        elemento.titulo.classList.add("erro")
+        elemento.titulo.setAttribute('placeholder', "Titulo inválido!")
         control = false
     }
     if(elemento.descricao.value.trim() == ""){
+        elemento.descricao.classList.add("erro")
+        elemento.descricao.setAttribute('placeholder', "Descrição inválida!")
         control = false
     }
     if(elemento.imgURL.value.trim() == ""){
+        elemento.imgURL.classList.add("erro")
+        elemento.imgURL.setAttribute('placeholder', "URL inválida!")
         control = false
     }
+    gerarErroCadastro()
     return control
 }
